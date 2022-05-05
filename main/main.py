bd0258b0769471e0bb965b8a237215ce9b89b94e
burnout87 <gabriele.barni@gmail.com>
2022-05-05 18:20:58 +0200

    removed not_needed property, Default graph node config

--- main/main.py
+++ main/main.py
@@ -19,7 +19,7 @@ st.title('Graph Quick-Look')
 def stream_graph():
     html_fn = 'graph_data/graph.html'
     ttl_fn = 'graph_data/graph.ttl'
-    graph_config_fn = 'graph_data/graph_config.json'
+    graph_config_fn_list = ['graph_data/graph_config.json']
 
     net = Network(
         height='750px', width='100%',
@@ -33,11 +33,25 @@ def stream_graph():
     fd = open(ttl_fn, 'r')
     graph_ttl_str = fd.read()
     fd.close()
+    # only the Default config
+    graph_config_obj = {"Default": {
+          "shape": "circle",
+          "color": "#111FFF",
+          "style": "filled",
+          "border": 0,
+          "cellborder": 0,
+          "value": 20,
+          "level": 0
+       }
+    }
 
-    with open(graph_config_fn) as graph_config_fn_f:
-        graph_config_obj = json.dumps(json.load(graph_config_fn_f))
+    for graph_config_fn in graph_config_fn_list:
+        with open(graph_config_fn) as graph_config_fn_f:
+            graph_config_obj.update(json.load(graph_config_fn_f))
 
-    graph_utils.add_js_click_functionality(net, html_fn, graph_ttl_stream=graph_ttl_str, graph_config_obj=graph_config_obj)
+    graph_config_obj_str = json.dumps(graph_config_obj)
+
+    graph_utils.add_js_click_functionality(net, html_fn, graph_ttl_stream=graph_ttl_str, graph_config_obj=graph_config_obj_str)
     graph_utils.set_html_content(net, html_fn)
     graph_utils.update_js_libraries(html_fn)
 
